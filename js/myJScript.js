450881d3088ce15ee7bd2e1818830731cf2c22be
MikhailMos <mos-mv@mail.ru>
2018-09-06 22:03:50 +0300

    исправлены некоторые ошибки

--- js/myJScript.js
+++ js/myJScript.js
@@ -1,8 +1,24 @@
-var linkMap = document.querySelector(".contacts-btn");
+var linkWriteUs  = document.querySelector(".contacts-btn");
+var popupWriteUs = document.querySelector(".modal-write-us");
+var closeWriteUs = popupWriteUs.querySelector(".write-us-btn-close");
+
+var linkMap  = document.querySelector(".contacts-map-link");
 var popupMap = document.querySelector(".modal-fullmap");
 var closeMap = popupMap.querySelector(".fullmap-btn-close");
 
-linkMap.addEventListener("click", function (evt) {
+// Форма обратной связи
+linkWriteUs.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popupWriteUs.classList.add("modal-show");
+});
+
+closeWriteUs.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popupWriteUs.classList.remove("modal-show");
+});
+
+// Карта
+linkMap.addEventListener("click", function (evt)) {
   evt.preventDefault();
   popupMap.classList.add("modal-show");
 });
